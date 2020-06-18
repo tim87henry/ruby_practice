2ae3fed7c1b0df37668a7dfcaffc5c8083ad5b3a
Timothy <tim87henry@gmail.com>
2020-06-18 23:38:03 +1000

    Caesor Cipher implemtation in Ruby

---
+++ caesar_cipher.rb
@@ -0,0 +1,25 @@
+def caesar_cipher(secret_str,num_shift)
+    encrypted_str ="" 
+    secret_str.each_byte do |c|
+        ascii = c
+        if (c >= 65 && c<=90) || (c >=97 && c <= 122)
+            if c <= 90
+                ascii_start = 65
+                ascii_end = 90
+            else
+                ascii_start = 97
+                ascii_end = 122
+            end
+            ascii = (c+num_shift.to_i) % ascii_end
+            ascii = ascii + ascii_start - 1 if ascii < ascii_start
+        end
+        encrypted_str += ascii.chr
+    end
+    puts encrypted_str
+end
+
+puts "Enter the string to be encrypted : "
+secret_str=gets.chomp
+puts "Enter the required shift factor : "
+num_shift=gets.chomp
+caesar_cipher(secret_str,num_shift)
