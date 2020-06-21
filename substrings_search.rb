c1d8b2de7244c96a9e85bfb815106b278b8cf3f0
Timothy <tim87henry@gmail.com>
2020-06-21 19:46:30 +1000

    Modified the search to find the right substrings

--- substrings_search.rb
+++ substrings_search.rb
@@ -1,7 +1,11 @@
 def substring(dictionary,search_string)
     result= Hash.new
-    search_string.split.each do |str|
-        result[str]=dictionary.count(str)
+    dictionary.each do |dict|
+        count=0
+        search_string.split.each do |str|
+            count+=1 if str.downcase.include?(dict.downcase)
+        end
+        result[dict]=count if count > 0
     end
     return result
 end
